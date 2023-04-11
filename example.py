f4e49f68c05bc0a99df6858c01811679eab7b66b
Tim Yang <thryang@outlook.com>
2023-04-11 02:38:50 -0400

    Custom Color Set Integration
    Added function `format` under class lumberjack. Renamed lumberjack to follow Python lower case. Added test scenarios to test `format` function. Changed how lumberjack's self.DICT functions, keeping color values from input.

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import pylumber as lum
 
 
-mainLog = lum.LUMBERJACK('test.log')
+mainLog = lum.lumberjack('test.log')
 
 mainLog.jack("Hello World!", "OK")
 mainLog.jack("Hello World!", "ERROR")
@@ -9,9 +9,14 @@ mainLog.jack("Hello World!", "DEBUG")
 
 
 testParameter = [
-    ("TEST", lum.ANSI_FORE.ACCENT1),
-    ("TEST2", lum.ANSI_FORE.ACCENT2),
-    ("TEST3", lum.ANSI_FORE.ACCENT3),
+    ("RED", lum.ANSI_FORE.ACCENT1),
+    ("GREEN", lum.ANSI_FORE.ACCENT2),
+    ("ORANGE", lum.ANSI_FORE.ACCENT3),
 ]
-testLog = lum.LUMBERJACK('test2.log', logParameter=testParameter)
-testLog.jack("Works?", "TEST")
+
+testLog = lum.lumberjack('test2.log', logParameter=testParameter)
+testLog.jack("Works?", "RED")
+testLog.jack("Works?", "GREEN")
+testLog.jack("Works?", "ORANGE")
+
+testLog.jack(testLog.format("Works?", "RED"), "RED")
--- pylumber/__init__.py
+++ pylumber/__init__.py
@@ -43,15 +43,6 @@ class CONST:
     PREFIX_SPACE = 8
     PREFIX_CHAR = " "
     PREFIX_MODI = [ANSI_MODI.BOLD]
-    LOG_LEVEL = {
-        # Definition for int Log Files:
-        0       : ("ERROR", ANSI_FORE.ACCENT1),
-        1       : ("OK", ANSI_FORE.ACCENT2),
-        2       : ("WARN", ANSI_FORE.ACCENT3),
-        3       : ("INFO", ANSI_FORE.ACCENT4),
-        4       : ("DEBUG", ANSI_FORE.ACCENT5)
-    }   
-
     DEFAULT_LOG_PARAM = [
         # Default Parameters for Log Files:
         ("ERROR", ANSI_FORE.ACCENT1),
@@ -60,53 +51,35 @@ class CONST:
         ("INFO", ANSI_FORE.ACCENT4),
         ("DEBUG", ANSI_FORE.ACCENT5)
     ]
-    
 
-
-
-class LOG_PREFIX:
-    # List of Modifications for Prefixes
-    INFO_MODI   = [ANSI_FORE.ACCENT4]
-    INFO        = "%s[%s]%s" %(''.join(CONST.PREFIX_MODI + INFO_MODI),
-                               "INFO".center(CONST.PREFIX_SPACE, CONST.PREFIX_CHAR),
-                               ANSI_MODI.END)
-    OK_MODI     = [ANSI_FORE.ACCENT2]     
-    OK          = "%s[%s]%s" %(''.join(CONST.PREFIX_MODI + OK_MODI),
-                              "OK".center(CONST.PREFIX_SPACE, CONST.PREFIX_CHAR), 
-                              ANSI_MODI.END)
-    WARN_MODI   = [ANSI_FORE.ACCENT3]
-    WARN        = "%s[%s]%s" %(''.join(CONST.PREFIX_MODI + WARN_MODI), 
-                              "WARN".center(CONST.PREFIX_SPACE, CONST.PREFIX_CHAR), 
-                              ANSI_MODI.END)
-    ERROR_MODI  = [ANSI_FORE.ACCENT1]
-    ERROR       = "%s[%s]%s" %(''.join(CONST.PREFIX_MODI + ERROR_MODI), 
-                              "ERROR".center(CONST.PREFIX_SPACE, CONST.PREFIX_CHAR), 
-                              ANSI_MODI.END)
-    DEBUG_MODI  = [ANSI_FORE.ACCENT5]
-    DEBUG       = "%s[%s]%s" %(''.join(CONST.PREFIX_MODI + DEBUG_MODI), 
-                               "DEBUG".center(CONST.PREFIX_SPACE, CONST.PREFIX_CHAR), 
-                               ANSI_MODI.END)
 
 # q: What is the regex expression for matching all sets between '\' and 'm'?
 # a: \033\[[0-9;]*m
 
-# Helper Function | Removes all ANSI Codes from String
+
+# Helper Function (Library Use) | Removes all ANSI Codes from String
 def STRIP_ANSI(string):
     return re.sub(r'\033\[[0-9;]*m', '', string)
 
-class LUMBERJACK:
+
+# Helper Function (User Use) | Formats string with 
+class lumberjack:
     def __init__(self, logFile=None, logParameter: list = CONST.DEFAULT_LOG_PARAM, logPrint=False):
         self.logFile = open(logFile, "w") if logFile else None
         self.DICT = {}
         for i in range(len(logParameter)):
             logPrefix = f"{logParameter[i][1]}[{logParameter[i][0].center(CONST.PREFIX_SPACE, CONST.PREFIX_CHAR)}]{ANSI_MODI.END}"
-            self.DICT[i] = logPrefix
-            self.DICT[logParameter[i][0]] = logPrefix
+            self.DICT[i] = (logPrefix, logParameter[i][1])                          # (Prefix w/ Formatting, ANSI Formatting) 
+            self.DICT[logParameter[i][0]] = (logPrefix, logParameter[i][1])         # (Additional Key for Prefix Input)
     
     def logWriter(self, msg):
         if self.logFile:
             self.logFile.write(STRIP_ANSI(msg + "\n"))
 
     def jack(self, msg: str, logLevel = "OK"):
-        self.logWriter(STRIP_ANSI(msg))
-        print(f"{self.DICT[logLevel]} {msg}{ANSI_MODI.END}")
+        modiMsg = f"{self.DICT[logLevel][0]} {msg}{ANSI_MODI.END}"
+        self.logWriter(modiMsg)
+        print(modiMsg)
+    
+    def format(self, msg: str, logLevel = "OK"):
+        return f"{self.DICT[logLevel][1]}{msg}{ANSI_MODI.END}"
